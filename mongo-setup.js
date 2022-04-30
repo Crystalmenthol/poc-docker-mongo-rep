807117a8b37094c5d613ec693ade6363507180b7
crystalmenthol <wutwut@odds.team>
2022-04-30 09:38:15 +0700

    poc

---
+++ mongo-setup.js
@@ -0,0 +1,22 @@
+rsConf = {
+  _id : "rs0",
+  members: [
+      {
+          "_id": 0,
+          "host": "mongodb1:27017",
+          "priority": 3
+      },
+      {
+          "_id": 1,
+          "host": "mongodb2:27017",
+          "priority": 2
+      },
+      {
+          "_id": 2,
+          "host": "mongodb3:27017",
+          "priority": 1
+      }
+  ]
+}
+
+rs.initiate(rsConf);
